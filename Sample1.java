38f4eb74caa6e9c54b784f04f87865c1b7a932c0
DemoTestUser1234 <135525836+DemoTestUser1234@users.noreply.github.com>
2023-06-04 21:11:22 +0530

    Update Sample1.java

--- Sample1.java
+++ Sample1.java
@@ -2,4 +2,5 @@ class Sample1
 {
   System.out.println("Hello World");
   System.out.println("Added new line successfully");
+   System.out.println("Added new line successfully from remote github repository");
 }
